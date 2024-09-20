790a6be14065a1230d46cfb0581a081f3d92bf17
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2024-09-21 02:55:56 +0530

    Time: 255 ms (9.88%) | Memory: 19.1 MB (13.42%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -8,11 +8,11 @@ public:
         int ans=0;
 
         for(int i=0;i<s.size();i++){
-            if(s[i]=='a'){vis[0]+=1; vis[0]=vis[0]%2;}
-            else if(s[i]=='e') {vis[1]+=1;  vis[1]=vis[1]%2;}
-            else if(s[i]=='i') {vis[2]+=1;  vis[2]=vis[2]%2;}
-            else if(s[i]=='o') {vis[3]+=1;  vis[3]=vis[3]%2;}
-            else if(s[i]=='u') {vis[4]+=1;  vis[4]=vis[4]%2;}
+            if(s[i]=='a')vis[0]^=1;
+            else if(s[i]=='e') vis[1]^=1;  
+            else if(s[i]=='i') vis[2]^=1;  
+            else if(s[i]=='o') vis[3]^=1;  
+            else if(s[i]=='u') vis[4]^=1;  
 
             if(mp.find(vis)!=mp.end()){
                 ans=max(ans,i-mp[vis]);
