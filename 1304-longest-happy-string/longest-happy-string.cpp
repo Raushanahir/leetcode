04c8d2adfff49fa26f462e8c8fdba8c7a74633b1
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2024-11-02 04:33:59 +0530

    Update longest-happy-string.cpp

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
-    string makeFancyString(string s) {
+    typedef pair<int,char>p;
+    string longestDiverseString(int a, int b, int c) {
         
         string ans="";
+        priority_queue<p,vector<p>>pq;
 
-        for(int i=0;i<s.size();){
-            int k=0;
-            string temp="";
-            while(s[i]==s[i+1] and i<s.size()){
-                i++;
-                k++;
-            }
-            if(k>=1){
-                ans+=s[i];
-                ans+=s[i];
-                i++;
+        if(a>0) pq.push({a,'a'});
+        if(b>0) pq.push({b,'b'});
+        if(c>0) pq.push({c,'c'});
+
+        while(!pq.empty()){
+            int curval=pq.top().first;
+            char curchar=pq.top().second;
+            pq.pop();
+
+            if(ans.size()>=2 and ans[ans.size()-1]==curchar and ans[ans.size()-2]==curchar){
+                // checking the last two character of string of ans if same then this condision will be used
+                if(pq.empty()) break;
+
+                int nextval=pq.top().first;
+                char nextchar=pq.top().second;
+                pq.pop();
+
+                nextval--;
+                ans+=nextchar;
+
+                if(nextval>0) 
+                    pq.push({nextval , nextchar});
             }
             else{
-                ans+=s[i];
-                i++;
+                ans+=curchar;
+                curval--;
             }
 
+            if(curval>0){
+                pq.push({curval,curchar});
+            }
         }
-
         return ans;
     }
 };
