6dcf533d54969173aa74f6b6f5e8610f3c9d2ff2
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-01-18 14:50:00 +0530

    Time: 72 ms (29.25%) | Memory: 19.1 MB (69.48%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,39 +1,58 @@
 class Solution {
 public:
     int minCost(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+
         pq.push({0, {0, 0}});
-        vector<vector<int>> shortest(n, vector<int>(m, 1e9));
-        shortest[0][0] = 0;
-        int delrow[] = {-1, 0, 1, 0};
-        int dir[] = {4, 1, 3, 2};
-        while(!pq.empty()) {
-            auto it = pq.top();
-            int row = it.second.first;
-            int col = it.second.second;
-            int cost = it.first;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        while (!pq.empty()) {
+            int step = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
+            int dir = grid[r][c];
             pq.pop();
-            if(row == n - 1 and col == m - 1) return cost;
-            for(int i = 0; i < 4; i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delrow[4 - i - 1];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
-                    if(grid[row][col] == dir[i]) {
-                        if(shortest[nrow][ncol] > cost) {
-                            shortest[nrow][ncol] = cost;
-                            pq.push({cost, {nrow, ncol}});
-                        }
-                    }
-                    else {
-                        if(shortest[nrow][ncol] > cost + 1) {
-                            shortest[nrow][ncol] = cost + 1;
-                            pq.push({cost + 1, {nrow, ncol}});
-                        }
-                    }
-                }
-            }
+
+            // Check if already visited
+            if (vis[r][c] == 1) continue;
+            vis[r][c] = 1;
+
+            // If reached the destination
+            if (r == n - 1 && c == m - 1) return step;
+
+            // Handle preferred direction
+            if (dir == 1 && c + 1 < m ) 
+                pq.push({step, {r, c + 1}});
+
+            else if (dir == 2 && c - 1 >= 0) 
+                pq.push({step, {r, c - 1}});
+            
+            else if (dir == 4 && r - 1 >= 0 )
+                pq.push({step, {r - 1, c}});
+            
+            else if (dir == 3 && r + 1 < n )
+                pq.push({step, {r + 1, c}});
+            
+
+            // Handle other directions with cost +1
+            if (dir != 3 && r + 1 < n )
+                pq.push({step + 1, {r + 1, c}});
+            
+            if (dir != 1 && c + 1 < m )
+                pq.push({step + 1, {r, c + 1}});
+            
+            if (dir != 2 && c - 1 >= 0 )
+                pq.push({step + 1, {r, c - 1}});
+            
+            if (dir != 4 && r - 1 >= 0 )
+                pq.push({step + 1, {r - 1, c}});
+            
         }
-        return 0;
+
+        return -1; // No path found (edge case)
     }
 };
