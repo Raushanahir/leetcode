56624d07c6b896c1fd5d828724037fbe76c71319
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-01-18 12:06:14 +0530

    Time: 27 ms (57.67%) | Memory: 18 MB (92.12%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int minCost(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        queue<pair<pair<int, int>, int>> q;
-        q.push({{0, 0}, 0});
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({0, {0, 0}});
         vector<vector<int>> shortest(n, vector<int>(m, 1e9));
         shortest[0][0] = 0;
         int delrow[] = {-1, 0, 1, 0};
         int dir[] = {4, 1, 3, 2};
-        while(!q.empty()) {
-            auto it = q.front();
-            int row = it.first.first;
-            int col = it.first.second;
-            int cost = it.second;
-            q.pop();
+        while(!pq.empty()) {
+            auto it = pq.top();
+            int row = it.second.first;
+            int col = it.second.second;
+            int cost = it.first;
+            pq.pop();
+            if(row == n - 1 and col == m - 1) return cost;
             for(int i = 0; i < 4; i++) {
                 int nrow = row + delrow[i];
                 int ncol = col + delrow[4 - i - 1];
@@ -21,18 +22,18 @@ public:
                     if(grid[row][col] == dir[i]) {
                         if(shortest[nrow][ncol] > cost) {
                             shortest[nrow][ncol] = cost;
-                            q.push({{nrow, ncol}, cost});
+                            pq.push({cost, {nrow, ncol}});
                         }
                     }
                     else {
                         if(shortest[nrow][ncol] > cost + 1) {
                             shortest[nrow][ncol] = cost + 1;
-                            q.push({{nrow, ncol}, cost + 1});
+                            pq.push({cost + 1, {nrow, ncol}});
                         }
                     }
                 }
             }
         }
-        return shortest[n - 1][m - 1];
+        return 0;
     }
 };
