44fdc8f6b7a252f6e2660e69f095cee60495f0b0
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-01-30 16:48:50 +0530

    Time: 13 ms (15.01%) | Memory: 28.6 MB (46.60%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int n,m;
+// dfs method for solving this problem
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
+        n=grid.size();
+        m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 and grid[i][j]==1){
+                    int k=dfs(i,j,grid,vis,1);
+                    ans=max(k,ans);
+                    cout << ans << " ";
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    int dfs(int r,int c,vector<vector<int>>&grid,vector<vector<int>>&vis,int area){
+
+        
+        int drow[]={1,0,0,-1};
+        int dcol[]={0,1,-1,0};
+        int k=0;
+        vis[r][c]=1;
+        for(int i=0;i<4;i++){
+            int row=drow[i]+r;
+            int col=dcol[i]+c;
+            if(row<n && col<m && row>=0 && col>=0 && vis[row][col]==0 && grid[row][col]==1){
+                area+=1;
+               k=dfs(row,col,grid,vis,area);
+               area=max(area,k);  // updating the value of area after traversing the grid;
+            }
+        }
+        return max(area,k);
+    }
+};
