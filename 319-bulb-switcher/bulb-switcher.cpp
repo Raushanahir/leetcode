c5657c9e96f80de23e4c93a53df2a959dd8abcd6
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-02-15 22:49:42 +0530

    Time: 1281 ms (4.11%) | Memory: 7.8 MB (35.57%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        if(n == 0) return 0;
         int ans = 0;
         for(int i = 1; i <=n; i++) {
             int val = ceil(sqrt(i) * 1.0);
