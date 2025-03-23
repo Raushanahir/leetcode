f572994bdd567fd1fcb6ec5f701840cc6467b171
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-03-23 14:56:47 +0530

    Time: 818 ms (38.86%) | Memory: 303.7 MB (38.38%) - LeetSync

--- 3809-properties-graph/properties-graph.cpp
+++ 3809-properties-graph/properties-graph.cpp
@@ -1,52 +1,67 @@
+class Disjoint_Set {
+public:
+    vector<int> parent, size;
+    Disjoint_Set(int n)
+    {
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] > size[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        } else {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+};
+
+bool check(vector<int> a, vector<int> b, int k) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    vector<int> v;
+    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(v));
+    int len = v.size();
+    for(int i = 1; i < v.size(); i++) {
+        if(v[i] == v[i - 1]) len--;
+    }
+    return len >= k;
+}
+
 class Solution {
 public:
-    // dsu;
-    int find(int x, vector<int>& par) {
-        if (x == par[x])
-            return x;
-
-        return par[x] = find(par[x], par);
-    }
-
-    void combine(int x, int y, vector<int>& par) {
-        int x_par = find(x, par);
-        int y_par = find(y, par);
-
-        if (x_par != y_par) {
-            par[y_par] = x_par;
-        }
-        return;
-    }
-
     int numberOfComponents(vector<vector<int>>& properties, int k) {
-
         int n = properties.size();
-
-        vector<int> par(n);
-        for (int i = 0; i < n; i++)
-            par[i] = i;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 1 + i; j < n; j++) {
-                if(intersect(properties[i], properties[j], k))                  combine(i, j, par);
+        Disjoint_Set ds(n);
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(check(properties[i], properties[j], k)) {
+                    ds.unionBySize(i, j);
+                }
             }
         }
-
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (par[i] == i) {
-                ans++;
-            }
+        for(int i = 0; i < n; i++) {
+            if(ds.findUPar(i) == i) ans++;
         }
-
         return ans;
-    }
-
-    bool intersect(vector<int> a, vector<int> b,int k) {
-        sort(a.begin(), a.end()); sort(b.begin(), b.end());
-        vector<int> v;
-        set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(v));
-        unordered_set<int> st(v.begin(), v.end());
-        return st.size() >= k;
     }
 };
