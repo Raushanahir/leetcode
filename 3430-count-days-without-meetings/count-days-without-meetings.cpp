c29786869584d95f9b0b65723ed32c51c03595aa
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-03-24 11:07:15 +0530

    Time: 76 ms (37.42%) | Memory: 127.4 MB (24.32%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        
-        sort(meetings.begin(),meetings.end());
 
-        int n=meetings.size();
-        stack<int>st;
-
-        for(int i=0;i<n;i++){
-            if(st.empty()){
+        sort(meetings.begin(), meetings.end());
+        int n = meetings.size();
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            if (st.empty() || st.top() < meetings[i][0]) {
                 st.push(meetings[i][0]);
                 st.push(meetings[i][1]);
-            }else{
-                if(st.top()>=meetings[i][0]){
-                    int top=st.top();
-                    st.pop();
-                    top=max(top,meetings[i][1]);
-                    st.push(top);
-                }
-                else{
-                    st.push(meetings[i][0]);
-                    st.push(meetings[i][1]);
-                }
+            } else {
+                int top = st.top();
+                st.pop();
+                top = max(top, meetings[i][1]);
+                st.push(top);
             }
         }
-
-        int prev=days+1;
-        int ans=0;
-        while(!st.empty()){
-            ans+=(prev-st.top()-1);
+        int prev = days + 1;
+        int ans = 0;
+        while (!st.empty()) {
+            ans += (prev - st.top() - 1);
             st.pop();
-            prev=st.top();
+            prev = st.top();
             st.pop();
         }
-        if(prev!=1){
-            ans+=prev-1;
+        if (prev != 1) {
+            ans += prev - 1;
         }
-
         return ans;
     }
 };
