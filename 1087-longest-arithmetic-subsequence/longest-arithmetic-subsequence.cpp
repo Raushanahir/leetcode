c9af59357c930bea4fe0ddb512ceb3c62c5b29c7
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-04-06 06:12:48 +0530

    Time: 103 ms (91.60%) | Memory: 145.7 MB (73.06%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,24 +1,21 @@
-// struct pair_hash {
-//     template <class T1, class T2>
-//     size_t operator()(const pair<T1, T2>& p) const {
-//         return hash<T1>()(p.first) ^ (hash<T2>()(p.second) << 1);
-//     }
-// };
-
 class Solution {
 public:
     int longestArithSeqLength(vector<int>& nums) {
-        int n = nums.size(), ans = 2;
-        vector<map<int, int>> dp(n);
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                int diff = nums[i] - nums[j];
-                int len = 2;
-                if(dp[j].find(diff) != dp[j].end()) len = dp[j][diff] + 1;
-                dp[i][diff] = max(dp[i][diff], len);
+        int n = nums.size();
+        int offset = 500; // to shift negative diffs to positive indices
+        int maxDiff = 1000; // range of diff: -500 to +500
+
+        vector<vector<int>> dp(n, vector<int>(maxDiff + 1, 1));
+        int ans = 2;
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int diff = nums[i] - nums[j] + offset;
+                dp[i][diff] = dp[j][diff] + 1;
                 ans = max(ans, dp[i][diff]);
             }
         }
+
         return ans;
     }
 };
