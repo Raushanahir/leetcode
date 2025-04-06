fe4ab84e8a5cf1ccccba1c1f8b19190319859283
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-04-06 05:50:40 +0530

    Time: 1203 ms (46.81%) | Memory: 248.2 MB (40.00%) - LeetSync

---
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -0,0 +1,24 @@
+// struct pair_hash {
+//     template <class T1, class T2>
+//     size_t operator()(const pair<T1, T2>& p) const {
+//         return hash<T1>()(p.first) ^ (hash<T2>()(p.second) << 1);
+//     }
+// };
+
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& nums) {
+        int n = nums.size(), ans = 2;
+        vector<unordered_map<int, int>> dp(n);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j];
+                int len = 2;
+                if(dp[j].count(diff)) len = dp[j][diff] + 1;
+                dp[i][diff] = max(dp[i][diff], len);
+                ans = max(ans, dp[i][diff]);
+            }
+        }
+        return ans;
+    }
+};
