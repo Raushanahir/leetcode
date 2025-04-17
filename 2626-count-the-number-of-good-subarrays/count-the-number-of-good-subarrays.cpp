45c191cad1c6d1b1b00b569026a54f55281ad9aa
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-04-17 07:26:46 +0530

    Time: 93 ms (30.97%) | Memory: 79.1 MB (54.61%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
-        int n = nums.size();
-        int same = 0, right = -1;
-        unordered_map<int, int> cnt;
-        long long ans = 0;
-        for (int left = 0; left < n; ++left) {
-            while (same < k && right + 1 < n) {
-                ++right;
-                same += cnt[nums[right]];
-                ++cnt[nums[right]];
+        
+        int i=0 , j=0;
+        int n=nums.size();
+        long long pair=0;
+        long long ans=0;
+        unordered_map<int,int>mp;
+        // 1 1 1 1 1 1
+        while(j<n){
+            mp[nums[j]]++;
+            if(mp[nums[j]]>1){
+                int p=mp[nums[j]]-1;
+                int t=mp[nums[j]];
+                pair+=(t*(t-1))/2-(p*(p-1))/2;
+                if(pair>=k){
+                    int flag=0;
+                    while(i<j and pair>=k){
+                        t=mp[nums[i]];
+                        p=mp[nums[i]]-1;
+                        mp[nums[i]]--;
+                        if(t>1)
+                        pair-=(t*(t-1))/2-(p*(p-1))/2;
+                        i++;
+                        flag++;
+                    }
+                    ans+=flag*(n-j);
+                }
             }
-            if (same >= k) {
-                ans += n - right;
-            }
-            --cnt[nums[left]];
-            same -= cnt[nums[left]];
+            j++;
         }
+
         return ans;
     }
 };
