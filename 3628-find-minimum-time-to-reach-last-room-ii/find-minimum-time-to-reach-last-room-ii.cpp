603138c5f0c7f6b71238664bd2ebf817a9627554
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-05-08 14:41:56 +0530

    Time: 221 ms (81.22%) | Memory: 97.3 MB (79.96%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& move) {
-        
+
         int n = move.size();
         int m = move[0].size();
         priority_queue<tuple<int, int, int, int>,
-                       vector<tuple<int,int ,int, int>>,
-                       greater<tuple<int,int ,int, int>>>
+                       vector<tuple<int, int, int, int>>,
+                       greater<tuple<int, int, int, int>>>
             pq;
-        // int inc=0;
-        pq.push({0,0,0,0});
+        pq.push({0, 0, 0, 0});
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
         vis[0][0] = 1;
 
         while (!pq.empty()) {
-            auto [time,oddeven, r, c] = pq.top();
-            // int time = it.first;
-            // int r = it.second.first;
-            // int c = it.second.second;
+            auto [time, oddeven, r, c] = pq.top();
             pq.pop();
 
             if (r == n - 1 and c == m - 1)
                 return time;
             int drow[4] = {1, -1, 0, 0};
             int dcol[4] = {0, 0, -1, 1};
-            // inc++;
 
             for (int i = 0; i < 4; i++) {
                 int row = r + drow[i];
@@ -33,13 +28,15 @@ public:
                 if (row < n and row >= 0 and col < m and col >= 0 and
                     vis[row][col] == 0) {
                     if (time > move[row][col]) {
-                        if(oddeven==0)
-                        pq.push({time +1 ,!oddeven,row, col});
-                        else pq.push({time +2 ,!oddeven,row, col});
+                        if (oddeven == 0)
+                            pq.push({time + 1, !oddeven, row, col});
+                        else
+                            pq.push({time + 2, !oddeven, row, col});
                     } else {
-                        if(oddeven==0)
-                        pq.push({1+move[row][col] ,!oddeven,row, col});
-                        else pq.push({2+move[row][col] ,!oddeven,row, col});
+                        if (oddeven == 0)
+                            pq.push({1 + move[row][col], !oddeven, row, col});
+                        else
+                            pq.push({2 + move[row][col], !oddeven, row, col});
                     }
                     vis[row][col] = 1;
                 }
