c41d5693757365e3652be69b230b0f8c74242c86
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-05-10 12:18:44 +0530

    Time: 254 ms (6.10%) | Memory: 137.4 MB (5.08%) - LeetSync

---
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    long long minSum(vector<int>& num1, vector<int>& num2) {
+
+        int n = num1.size(), m = num2.size();
+        sort(num1.rbegin(), num1.rend());
+        sort(num2.rbegin(), num2.rend());
+        long long zeros1 = -1, zeros2 = -1;
+
+        long long sum1 = 0, sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += num1[i];
+            if (num1[i] == 0) {
+                zeros1 = n - i;
+                break;
+            }
+        }
+
+        for (int j = 0; j < m; j++) {
+            sum2 += num2[j];
+            if (num2[j] == 0) {
+                zeros2 = m - j;
+                break;
+            }
+        }
+
+        cout << zeros1 << " " << zeros2 << endl ;
+        cout << sum1 << " " << sum2 ;
+        if (zeros1 == -1 || zeros2 == -1) {
+            if (zeros1 != -1) {
+                sum1 += zeros1;
+                return sum2>=sum1? sum2:-1; 
+            } else if (zeros2 != -1) {
+                sum2 += zeros2;
+                return sum1>=sum2?sum1:-1;
+            }
+            if (sum1 == sum2) {
+                return sum1;
+            } else
+                return -1;
+        } else {
+            sum1 += zeros1;
+            sum2 += zeros2;
+        }
+        return max(sum1, sum2);
+    }
+};
