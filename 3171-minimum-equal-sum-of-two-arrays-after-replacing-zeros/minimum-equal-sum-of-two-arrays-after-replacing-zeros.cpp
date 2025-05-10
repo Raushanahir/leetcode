476c2210daca953d8eb17ba3168ba9fef4d06241
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-05-10 12:25:42 +0530

    Time: 124 ms (81.69%) | Memory: 135.3 MB (73.56%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -3,39 +3,32 @@ public:
     long long minSum(vector<int>& num1, vector<int>& num2) {
 
         int n = num1.size(), m = num2.size();
-        sort(num1.rbegin(), num1.rend());
-        sort(num2.rbegin(), num2.rend());
-        long long zeros1 = -1, zeros2 = -1;
+        long long zeros1 = 0, zeros2 = 0;
 
         long long sum1 = 0, sum2 = 0;
         for (int i = 0; i < n; i++) {
             sum1 += num1[i];
             if (num1[i] == 0) {
-                zeros1 = n - i;
-                break;
+                zeros1++;
             }
         }
 
         for (int j = 0; j < m; j++) {
             sum2 += num2[j];
             if (num2[j] == 0) {
-                zeros2 = m - j;
-                break;
+                zeros2++;
             }
         }
 
-        if (zeros1 == -1 || zeros2 == -1) {
-            if (zeros1 != -1) {
+        if (zeros1 == 0 || zeros2 ==0) {
+            if (zeros1 != 0) {
                 sum1 += zeros1;
                 return sum2 >= sum1 ? sum2 : -1;
-            } else if (zeros2 != -1) {
+            } else if (zeros2 != 0) {
                 sum2 += zeros2;
                 return sum1 >= sum2 ? sum1 : -1;
             }
-            if (sum1 == sum2) {
-                return sum1;
-            } else
-                return -1;
+            return sum1==sum2?sum1: -1;
         } else {
             sum1 += zeros1;
             sum2 += zeros2;
