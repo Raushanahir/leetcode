6a3765958c6b3929a4b6c6656f67a3d0cdab4a1a
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-08-05 00:46:59 +0530

    Time: 1851 ms (5.25%) | Memory: 69.8 MB (98.21%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+
+        int a = 0, b = -1;
+        int j = 1, i = 0;
+        int ans = 0;
+        int n = fruits.size();
+        while (j < n and fruits[j] == fruits[j - 1]) {
+            a = j;
+            j++;
+        }
+        b = j;
+
+        while (j < n) {
+            cout << a << " " << b << endl;
+            if (fruits[j] != fruits[a] and fruits[j] != fruits[b]) {
+                ans = max(ans, j - i);
+                if (a < b) {
+                    while (i <= a) {
+                        i++;
+                    }
+                    a = j;
+                } else {
+                    while (i <= b) {
+                        i++;
+                    }
+                    b = j;
+                }
+            } else {
+                if (fruits[j] == fruits[a])
+                    a = j;
+                else
+                    b = j;
+            }
+            j++;
+        }
+        ans = max(ans, j - i);
+
+        return ans;
+    }
+};
