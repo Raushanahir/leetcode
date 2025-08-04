bcef9f1b23a89f3a6692bc42cac682192f823a59
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-08-05 00:50:48 +0530

    Time: 36 ms (71.96%) | Memory: 75.1 MB (31.99%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,36 +2,21 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
 
-        int a = 0, b = -1;
-        int j = 1, i = 0;
+        int j = 0, i = 0;
         int ans = 0;
-        int n = fruits.size();
-        while (j < n and fruits[j] == fruits[j - 1]) {
-            a = j;
-            j++;
-        }
-        b = j;
+        int n = fruits.size();\
+        unordered_map<int,int>mp;
 
         while (j < n) {
-            if (fruits[j] != fruits[a] and fruits[j] != fruits[b]) {
-                ans = max(ans, j - i);
-                if (a < b) {
-                    while (i <= a) {
-                        i++;
+            mp[fruits[j]]++;
+                ans=max(ans,j-i);
+                while(mp.size()>2){
+                    mp[fruits[i]]--;
+                    if(mp[fruits[i]]==0){
+                        mp.erase(fruits[i]);
                     }
-                    a = j;
-                } else {
-                    while (i <= b) {
-                        i++;
-                    }
-                    b = j;
+                    i++;
                 }
-            } else {
-                if (fruits[j] == fruits[a])
-                    a = j;
-                else
-                    b = j;
-            }
             j++;
         }
         ans = max(ans, j - i);
