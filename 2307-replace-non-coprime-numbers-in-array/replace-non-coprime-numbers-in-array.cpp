a1de1c9ccce763368118eda5e09e905c486accad
Raushan Kumar <132647521+Raushanahir@users.noreply.github.com>
2025-09-17 03:07:51 +0530

    Time: 30 ms (29.95%) | Memory: 128.2 MB (29.95%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -1,34 +1,38 @@
 class Solution {
-    int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-int lcm(int a, int b) {
-    return (a / gcd(a, b)) * b;
-}
 public:
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        vector<int> ans;
-        for (int x : nums) {
-            ans.push_back(x);
-            // keep merging as long as last two are non-coprime
-            while (ans.size() > 1) {
-                int a = ans[ans.size()-2];
-                int b = ans[ans.size()-1];
-                int g = gcd(a, b);
-                if (g > 1) {
-                    ans.pop_back();
-                    ans.pop_back();
-                    ans.push_back(lcm(a, b));
-                } else {
-                    break;
+
+        int n = nums.size();
+        stack<int> st;
+        st.push(nums[0]);
+        int k = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (!st.empty())
+                k = gcd(st.top(), nums[i]);
+            if (st.empty() || k == 1)
+                st.push(nums[i]);
+            else {
+                int kk = nums[i];
+                while (!st.empty() and k > 1) {
+                    kk = (1LL * st.top() * kk) / k;
+                    st.pop();
+                    if (!st.empty())
+                        k = gcd(st.top(), kk);
                 }
+                st.push(kk);
             }
         }
+
+        vector<int> ans;
+
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
         return ans;
     }
 };
